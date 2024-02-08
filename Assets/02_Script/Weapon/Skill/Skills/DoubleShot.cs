c7d1a0914bbff0625567cd5e2c3520932f2f5580
ahslxjvhapt12 <likjhee1@gmail.com>
2024-02-08 18:00:28 +0900

    스킬 제작 시작

---
+++ Assets/02_Script/Weapon/Skill/Skills/DoubleShot.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class DoubleShot : Skill
+{
+
+    [SerializeField] private Bullet bullet;
+
+    public override void Excute(Transform weaponTrm, Transform target, int power)
+    {
+
+
+
+    }
+
+}
