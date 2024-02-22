3ac21eb934bbc901a2ec16d673078839d16ad8a3
준이 <98876187+June060522@users.noreply.github.com>
2024-02-22 16:08:42 +0900

    bug fix

--- Assets/02_Script/RandomMap/CustomRoom.cs
+++ Assets/02_Script/RandomMap/CustomRoom.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
-public class CustomRoom
+public class CustomRoom : MonoBehaviour
 {
     public Tilemap wallTilemap;
     public Tilemap tilemap;
