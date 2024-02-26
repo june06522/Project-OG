bed78fbaf4c2ce90be8ed3c6bea8a359d216778b
ahslxjvhapt12 <likjhee1@gmail.com>
2024-02-26 18:04:19 +0900

    Tile

--- Assets/02_Script/Weapon/Skill/WeaponAbilityManager.cs
+++ Assets/02_Script/Weapon/Skill/WeaponAbilityManager.cs
@@ -7,12 +7,8 @@ using UnityEngine;
 public enum Abillity
 {
     Piercing,
-<<<<<<< Updated upstream
-
-=======
     Explosion,
     Thunder,
->>>>>>> Stashed changes
 }
 
 public class WeaponAbilityManager : MonoBehaviour
