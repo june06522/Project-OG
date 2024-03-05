aafcbb2727e72596993900b5bd58033baa5b4a99
ahslxjvhapt12 <likjhee1@gmail.com>
2024-03-05 20:27:35 +0900

    Kunai,Sickle

---
+++ Assets/02_Script/Weapon/Skill/Skills/Kunai/ShadowKunai.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 잔상 추가 공격
+public class ShadowKunai : Skill
+{
+
+    public override void Excute(Transform weaponTrm, Transform target, int power)
+    {
+
+
+
+    }
+
+}
