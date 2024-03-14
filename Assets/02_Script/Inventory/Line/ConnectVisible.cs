d640883c7eab014b27a875a90871991e09bf1892
준이 <98876187+June060522@users.noreply.github.com>
2024-03-14 20:05:20 +0900

    connect

--- Assets/02_Script/Inventory/Line/ConnectVisible.cs
+++ Assets/02_Script/Inventory/Line/ConnectVisible.cs
@@ -73,28 +73,36 @@ public class ConnectVisible : MonoBehaviour
 
     void Connect(LineRenderer line, Vector2 pos, Vector2Int dir, Vector2 originpos, Hashtable isVisited)
     {
-        Vector2 tempVec = originpos - new Vector2(dir.x * -0.93f, dir.y * -0.93f);
+        Vector2 tempVec = originpos + new Vector2(dir.x * 0.93f, dir.y * 0.93f);
 
-        //스택 오버 플로우 방지 <- 방문한곳 체크
+        #region 스택 오버 플로우 방지 <- 방문한곳 체크
         if (isVisited.ContainsKey(pos) && (bool)isVisited[pos])
             return;
 
         isVisited[pos] = true;
-        Stack<BrickPoint> s = new Stack<BrickPoint>();
+        #endregion
+
+        //Stack<BrickPoint> s = new Stack<BrickPoint>();
 
         Vector2Int fillCheckVec = new Vector2Int((int)pos.x, (int)pos.y);
+
+
+
         if (inventory.CheckFill2(fillCheckVec))
         {
             InventoryObjectData data = inventory.GetObjectData2(fillCheckVec, dir);
             if (data != null)
             {
+
+                BrickPoint b = new BrickPoint();
                 bool isConnect = false;
 
-                //무기만 예외처리하는 일종 하드코딩 ㅎㅎ...
+                #region 무기 예외처리
                 if (data.sendPoints.Count == 0)
                     isConnect = true;
+                #endregion
 
-                //연결 되었는지 확인
+                #region 연결된 블록 있으면 스택에 추가 없으면 리턴
                 foreach (var point in data.inputPoints)
                 {
                     if (point.dir == dir && point.point == fillCheckVec - data.originPos)
@@ -103,10 +111,9 @@ public class ConnectVisible : MonoBehaviour
                         {
                             if (point.point == point1.point)
                             {
-                                BrickPoint b;
                                 b.point = point.point;
                                 b.dir = point1.dir;
-                                s.Push(b);
+                                //s.Push(b);
                             }
                         }
                         isConnect = true;
@@ -114,55 +121,64 @@ public class ConnectVisible : MonoBehaviour
                 }
 
                 if (!isConnect) return;
+                #endregion
 
                 //라인 렌더러에 추가
                 AddLineRenderPoint(line, tempVec);
 
-                if (s.Count == 0)
+                //#region 무기면 리턴
+                if (b.dir == null)
                     return;
-
-                do
-                {
-                    BrickPoint tmpVec = s.Pop(); // 0,1  아래 오른쪽
-                                                 // 만약 이어진 블럭이 sendpoint면 데이터 보내기
-                    foreach (SignalPoint point1 in data.sendPoints)
-                    {
-                        if (point1.point == tmpVec.point)
-                        {
-                            Vector2 tempPos1 = data.originPos + point1.dir + point1.point;
-                            Connect(line, tempPos1, point1.dir, tempVec - new Vector2(point1.point.x * -0.93f, point1.point.y * -0.93f), isVisited);
-                        }
-                    }
-                    //이친구와 이어진 블록 연결
-                    foreach (BrickPoint point in data.bricks)
-                    {
-                        //방향검사
-                        foreach (var v in tmpVec.dir)
-                        {
-                            //전블록과 이어지면 연결
-                            if (point.point == tmpVec.point + v)// 0,0 1,1
-                            {
-                                Vector2 tempPos = data.originPos + point.point;
-
-                                if (isVisited.ContainsKey(tempPos) && (bool)isVisited[tempPos])
-                                    continue;
-
-                                isVisited[tempPos] = true;
-
-                                AddLineRenderPoint(line,
-                                    tempVec - new Vector2(point.point.x * -0.93f, point.point.y * -0.93f));
-
-                                BrickPoint b;
-                                b.point = point.point;
-                                b.dir = point.dir;
-                                s.Push(b);
-                            }
-                        }
-                    }
-                } while (s.Count > 0);
+                //#endregion
+                
+                BrickCircuit(b,tempVec, line, data, isVisited);
             }
         }
-        isVisited[pos] = false;
+    }
+
+    private void BrickCircuit(BrickPoint tmpVec, Vector2 tempVec, LineRenderer line, InventoryObjectData data, Hashtable isVisited)
+    {
+        #region 연결된 곳 순회
+        //BrickPoint tmpVec = s.Pop();
+        foreach (SignalPoint point1 in data.sendPoints)
+        {
+            if (point1.point == tmpVec.point)
+            {
+                Vector2 tempPos1 = data.originPos + point1.dir + point1.point;
+                Connect(line, tempPos1, point1.dir, tempVec, isVisited);
+            }
+        }
+        //이친구와 이어진 블록 연결
+        foreach (BrickPoint point in data.bricks)
+        {
+            //방향검사
+            foreach (var v in tmpVec.dir)
+            {
+                //전블록과 이어지면 연결
+                if (point.point == tmpVec.point + v)// 0,0 1,1
+                {
+                    Vector2 tempPos = data.originPos + point.point;
+
+                    if (isVisited.ContainsKey(tempPos) && (bool)isVisited[tempPos])
+                        continue;
+
+                    isVisited[tempPos] = true;
+
+
+                    tempVec += new Vector2(v.x * 0.93f, v.y * 0.93f);
+                    AddLineRenderPoint(line, tempVec);
+
+                    BrickPoint b;
+                    b.point = point.point;
+                    b.dir = point.dir;
+                    BrickCircuit(b, tempVec, line, data, isVisited);
+                    tempVec -= new Vector2(v.x * 0.93f, v.y * 0.93f);
+                    AddLineRenderPoint(line, tempVec);
+                    //s.Push(b);
+                }
+            }
+        }
+        #endregion
     }
 
     private void AddLineRenderPoint(LineRenderer line, Vector3 pos)
