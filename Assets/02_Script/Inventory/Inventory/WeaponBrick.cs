8b8619ea93cbf3286329821bb84d999d2af0946a
ahslxjvhapt12 <likjhee1@gmail.com>
2024-03-21 10:15:26 +0900

    .
    .

--- Assets/02_Script/Inventory/Inventory/WeaponBrick.cs
+++ Assets/02_Script/Inventory/Inventory/WeaponBrick.cs
@@ -71,8 +71,9 @@ public class WeaponBrick : InvenBrick
 
     private string[] GetOnSkillList()
     {
-        List<string> list = InventoryWeaponInfo.Instance.GetConnect(InvenObject.originPos.x, InvenObject.originPos.y);
+        //List<string> list = InventoryWeaponInfo.Instance.GetConnect(InvenObject.originPos.x, InvenObject.originPos.y);
 
-        return list.ToArray();
+        //return list.ToArray();
+        return null;
     }
 }
