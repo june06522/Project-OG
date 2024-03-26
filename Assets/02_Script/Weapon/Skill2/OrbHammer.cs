57a36848ac54b4ddec8e7f5a4a298874565b62ac
ahslxjvhapt12 <likjhee1@gmail.com>
2024-03-26 18:29:56 +0900

    ㅎㅎ 안올렸네

---
+++ Assets/02_Script/Weapon/Skill2/OrbHammer.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbHammer : Skill
+{
+
+    [SerializeField] GameObject hammerEffect;
+ 
+    public override void Excute(Transform weaponTrm, Transform target, int power)
+    {
+        float angle = Mathf.Atan2(weaponTrm.right.y, weaponTrm.right.x) * Mathf.Rad2Deg;
+
+        var temp = Instantiate(hammerEffect, weaponTrm.position, Quaternion.Euler(0, 0, angle - 90));
+
+        temp.GetComponent<OrbCollision>().SetDamage(power * 10);
+        temp.GetComponent<Bullet>().Shoot();
+        Destroy(temp, 1.6f);
+    }
+
+}
