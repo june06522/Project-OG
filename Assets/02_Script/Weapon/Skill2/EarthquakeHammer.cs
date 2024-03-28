3581aade90d10e33549cb16a49d67168bc98c235
ahslxjvhapt12 <likjhee1@gmail.com>
2024-03-28 19:16:38 +0900

    Skill Visual Update

--- Assets/02_Script/Weapon/Skill2/EarthquakeHammer.cs
+++ Assets/02_Script/Weapon/Skill2/EarthquakeHammer.cs
@@ -10,6 +10,7 @@ public class EarthquakeHammer : Skill
         float angle = Mathf.Atan2(weaponTrm.right.y, weaponTrm.right.x) * Mathf.Rad2Deg;
 
         var temp = Instantiate(hammerEffect, weaponTrm.position, Quaternion.Euler(0, 0, angle - 90));
+        temp.transform.localScale = Vector3.one + Vector3.one * power * 0.2f;
 
         temp.GetComponent<CrackCollision>().SetDamage(power * 10);
         Destroy(temp, 1.6f);
