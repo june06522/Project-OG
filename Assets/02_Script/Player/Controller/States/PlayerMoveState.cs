d929824e8ff9a6e05885f1c00405b9c2be3607a9
ahslxjvhapt12 <likjhee1@gmail.com>
2024-04-09 00:00:49 +0900

    Visual

--- Assets/02_Script/Player/Controller/States/PlayerMoveState.cs
+++ Assets/02_Script/Player/Controller/States/PlayerMoveState.cs
@@ -7,17 +7,19 @@ using DG.Tweening;
 public class PlayerMoveState : PlayerRootState
 {
     PlayerController playerController;
+    GameObject _visual;
     bool increase = false;
 
-    public PlayerMoveState(PlayerController controller) : base(controller)
+    public PlayerMoveState(PlayerController controller, GameObject visual) : base(controller)
     {
         playerController = controller;
+        this._visual = visual;
     }
 
     protected override void EnterState()
     {
         playerController.moveParticle.Play();
-        transform.DOScale(new Vector2(0.8f, 1.3f), 0.2f).SetEase(Ease.InOutBack);
+        _visual.transform.DOScale(new Vector2(0.8f, 1.3f), 0.2f).SetEase(Ease.InOutBack);
     }
 
     protected override void UpdateState()
@@ -28,28 +30,28 @@ public class PlayerMoveState : PlayerRootState
         float rotateDegree = Mathf.Atan2(inputController.MoveDir.y, inputController.MoveDir.x) * Mathf.Rad2Deg;
 
         Quaternion targetRotation = Quaternion.Euler(0f, 0f, rotateDegree - 90);
-        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.deltaTime * 15);
+        _visual.transform.rotation = Quaternion.Lerp(_visual.transform.rotation, targetRotation, Time.deltaTime * 15);
 
         if (increase)
         {
             // upscale
-            transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y + Time.deltaTime * 3, transform.localScale.z);
+            _visual.transform.localScale = new Vector3(_visual.transform.localScale.x, _visual.transform.localScale.y + Time.deltaTime * 3, _visual.transform.localScale.z);
         }
         else
         {
-            transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y - Time.deltaTime * 3, transform.localScale.z);
+            _visual.transform.localScale = new Vector3(_visual.transform.localScale.x, _visual.transform.localScale.y - Time.deltaTime * 3, _visual.transform.localScale.z);
         }
 
-        if (transform.localScale.y < 1.2f)
+        if (_visual.transform.localScale.y < 1.2f)
         {
             increase = true;
         }
-        if (transform.localScale.y > 1.4f)
+        if (_visual.transform.localScale.y > 1.4f)
         {
             increase = false;
         }
 
-        rigid.velocity = transform.up * playerData.MoveSpeed;
+        rigid.velocity = _visual.transform.up * playerData.MoveSpeed;
     }
 
 
@@ -58,9 +60,9 @@ public class PlayerMoveState : PlayerRootState
 
         playerController.moveParticle.Stop();
         DOTween.Sequence()
-            .Append(transform.DOScale(new Vector2(1.3f, 0.8f), 0.2f).SetEase(Ease.InOutBack))
+            .Append(_visual.transform.DOScale(new Vector2(1.3f, 0.8f), 0.2f).SetEase(Ease.InOutBack))
             //.Append(transform.DOScale(Vector3.one * 1.4f, 0.2f).SetEase(Ease.OutBounce))
-            .Append(transform.DOScale(Vector3.one, 0.3f).SetEase(Ease.OutBounce));
+            .Append(_visual.transform.DOScale(Vector3.one, 0.3f).SetEase(Ease.OutBounce));
 
         rigid.velocity = Vector2.zero;
 
