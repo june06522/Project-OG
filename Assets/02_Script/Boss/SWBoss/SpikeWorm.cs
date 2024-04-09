76cfb13fd6f1aba3d4d76a094d1846926c783321
ksan09 <kansan060223@gmail.com>
2024-04-09 20:09:30 +0900

    SpikeWorm

---
+++ Assets/02_Script/Boss/SWBoss/SpikeWorm.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeWorm : Boss
+{
+
+
+
+
+
+
+
+    protected override void OnEnable()
+    {
+        base.OnEnable();
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+    }
+
+}
