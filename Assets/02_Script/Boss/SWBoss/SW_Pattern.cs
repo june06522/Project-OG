6c796034131b26cc4e2b32674af96f24d75119be
ksan09 <kansan060223@gmail.com>
2024-04-12 14:01:18 +0900

    Add BossPattern

--- Assets/02_Script/Boss/SWBoss/SW_Pattern.cs
+++ Assets/02_Script/Boss/SWBoss/SW_Pattern.cs
@@ -1,14 +1,21 @@
+using DG.Tweening;
+using FD.Dev;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 using UnityEngine.Playables;
+using Random = UnityEngine.Random;
 
 public enum ESpikeWormState
 {
     None,
     Cool,
     SnakeMove,
+    SpikeBullet,
+    EnergeCharge,
+    TightSnakeBody
 
 }
 
@@ -17,44 +24,124 @@ public class SW_Pattern : MonoBehaviour
     [Header("Boss")]
     private Transform _player;
     [SerializeField]
-    ESpikeWormState state = ESpikeWormState.None;
+    ESpikeWormState _state = ESpikeWormState.None;
+    private bool _isOutside = true;
+
+    [Header("Body")]
+    [SerializeField]
+    private Transform _head;
+    [SerializeField]
+    private SpriteRenderer _headSpriteRenderer;
+    private readonly int HASH_BLINK = Shader.PropertyToID("_StrongTintFade");
 
     [Header("Info")]
     [SerializeField]
     private SnakeMove _snakeMove;
     [SerializeField]
     private LayerMask _obstacleLayer;
+    [SerializeField]
+    private int _bodyCount;
 
     [Header("SnakeMove Pattern")]
     [SerializeField]
-    private Transform _head;
-    [SerializeField]
-    private float _radius;
-    [SerializeField]
-    private Transform _startPos;
+    private float _detectRadius;
 
+    [Header("EnergeCharge Pattern")]
+    [SerializeField]
+    private Charge_SWPart _chargeObject;
+    [SerializeField]
+    private float _chargeObjectSpeed;
+    [SerializeField]
+    private float _chargeObjectDamage;
+    [SerializeField]
+    private LayerMask _collectedLayer;
+
+    private int _currentBodyCount = 0;
+
+    [Header("Spike Bullet")]
+    [SerializeField]
+    private EnemyBullet _spikeBullet;
+    [SerializeField]
+    private int _shotCount = 5;
+    [SerializeField]
+    private float _turnSpeed = 5f;
+
+    [SerializeField]
+    private GameObject _dangerCrossObject;
+    [SerializeField]
+    private GameObject _dangerXCrossObject;
+
+    private bool _isShotCross = false;
+    private int _currentShotCount = 0;
+
+    [Header("Stage Pos")]
+    [SerializeField]
+    private Transform _underStartPos;
+    [SerializeField]
+    private Transform _worldCenterPos;
+
+    // move Value
+    private Vector3 _movePos = Vector3.zero;
     private Vector3 _dir = Vector3.up;
-    private float _speed = 10.0f;
+    private float _speed = 15.0f;
+
+    // state Value
     private float _cool = 0f;
+
+    // tightSnakeMove
+    private float _angle = 0f;
+    private float _moveAngle = 0f;
+    private float _tightSnakeMoveSpeed = 30.0f;
+    private float _rotateAngleSpeed = 180.0f;
+    Color _saveColor;
 
     private void Awake()
     {
         _player = GameManager.Instance.player;
+        _saveColor = _headSpriteRenderer.color;
     }
 
     private void Update()
     {
         BossStatePlay();
+        RandomState();
+    }
 
-        if(Input.GetKeyDown(KeyCode.C))
+    public void RandomState()
+    {
+        if (_state != ESpikeWormState.None)
+            return;
+
+        if(_isOutside)
         {
-            SetBossState(ESpikeWormState.SnakeMove);
+            int rand = Random.Range(0, 2);
+            if(rand == 0)
+            {
+                SetBossState(ESpikeWormState.TightSnakeBody);
+            }
+            else if(rand == 1)
+            {
+                SetBossState(ESpikeWormState.SnakeMove);
+            }
+        }
+        else
+        {
+            int rand = Random.Range(0, 2);
+            if(rand == 0)
+            {
+                SetBossState(ESpikeWormState.EnergeCharge);
+            }
+            else if(rand == 1)
+            {
+                SetBossState(ESpikeWormState.SpikeBullet);
+            }
+            
         }
     }
 
     private void BossStatePlay()
     {
-        switch (state)
+        switch (_state)
         {
             case ESpikeWormState.None:
                 //
@@ -65,9 +152,57 @@ public class SW_Pattern : MonoBehaviour
             case ESpikeWormState.SnakeMove:
                 SnakeMovePattern();
                 break;
+            case ESpikeWormState.SpikeBullet:
+                SpikeBulletPattern();
+                break;
+            case ESpikeWormState.TightSnakeBody:
+                TightSnakePattern();
+                break;
+            case ESpikeWormState.EnergeCharge:
+                EnergeChargePattern();
+                break;
         }
     }
 
+    private void EnergeChargePattern()
+    {
+        // Move
+        if(_currentBodyCount < _bodyCount)
+        {
+            if(Vector3.Distance(_head.position, _movePos) < 0.2f)
+            {
+                _angle += 5f * Mathf.Deg2Rad;
+                _movePos = _worldCenterPos.position + new Vector3(Mathf.Cos(_angle), Mathf.Sin(_angle), 0) * (10f + _currentBodyCount * 0.2f);
+            }
+
+            _dir = (_movePos - _head.position).normalized;
+            _head.position += _dir * _speed * Time.deltaTime;
+
+            Collider2D col = Physics2D.OverlapCircle(_head.position, _detectRadius, _collectedLayer);
+            if (col)
+            {
+                Destroy(col.gameObject);
+                ChargeCount();
+            }
+
+        }
+        else // OutWorld
+        {
+            if(Vector3.Distance(_head.position, _worldCenterPos.position) > 15f)
+            {
+                _moveAngle = _angle + (360f * Mathf.Deg2Rad);
+
+                _isOutside = true;
+                _currentBodyCount = 0;
+                _state = ESpikeWormState.TightSnakeBody;
+                Vector3 pos = _worldCenterPos.position + new Vector3(Mathf.Cos(_angle), Mathf.Sin(_angle), 0) * 14.9f;
+                _dir = (pos - _head.position).normalized;
+            }
+
+            _head.position += _dir * _speed * 2 * Time.deltaTime;
+        }
+
+    }
     private void SnakeMovePattern()
     {
         _head.position += _speed * _dir * Time.deltaTime;
@@ -75,7 +210,8 @@ public class SW_Pattern : MonoBehaviour
         if(_dir == Vector3.up || _dir == Vector3.down)
         {
             // bossY == playerY
-            if (Mathf.Abs(_head.position.y - _player.position.y) < 0.2f)
+            
+            if ((_head.position.y - _player.position.y) *_dir.y > 0)
             {
 
                 if (_head.position.x < _player.position.x)
@@ -95,7 +231,7 @@ public class SW_Pattern : MonoBehaviour
         }
         else
         {
-            if (Mathf.Abs(_head.position.x - _player.position.x) < 0.2f)
+            if ((_head.position.x - _player.position.x) * _dir.x > 0f)
             {
 
                 if (_head.position.y < _player.position.y)
@@ -115,12 +251,97 @@ public class SW_Pattern : MonoBehaviour
 
         }
         
-        if(Physics2D.OverlapCircle(_head.position, _radius, _obstacleLayer))
+        if(Physics2D.OverlapCircle(_head.position, _detectRadius, _obstacleLayer))
         {
+            if (coroutine != null)
+                StopCoroutine(coroutine);
+            coroutine = StartCoroutine(BlinkCo(0.5f));
+            Sequence seq = DOTween.Sequence();
+            seq.Append(transform.DOScale(Vector3.one * 1.5f, 0.2f).SetEase(Ease.OutElastic));
+            seq.Append(transform.DOScale(Vector3.one, 0.2f).SetEase(Ease.InBounce));
+
+            _isOutside = false;
             _snakeMove.DestroyBody();
-            SetCoolTime(10f);
+            SetCoolTime(5f);
         }
 
+
+    }
+    private void SpikeBulletPattern()
+    {
+        // 
+        if (_shotCount <= _currentShotCount)
+        {
+            _currentShotCount = 0;
+            SetCoolTime(1f);
+        }
+
+        if (Vector3.Distance(_head.position, _movePos) < 0.2f)
+        {
+            _angle += -75f * Mathf.Deg2Rad;
+            _movePos = _worldCenterPos.position + new Vector3(Mathf.Cos(_angle), Mathf.Sin(_angle), 0) * 5f;
+            _dir = (_movePos - _head.position).normalized;
+
+            //Shot Bullet
+            (_isShotCross ? _dangerCrossObject : _dangerXCrossObject).SetActive(true);
+
+            //Anim
+            FAED.InvokeDelay(() =>
+            {
+                (_isShotCross ? _dangerCrossObject : _dangerXCrossObject).SetActive(false);
+
+                float offsetAngle = (_isShotCross) ? 0f : 45f;
+                for (int i = 0; i < 4; ++i)
+                {
+                    float shotAngle = (i * 90 + offsetAngle) * Mathf.Deg2Rad;
+                    Vector2 shotDir = new Vector2(Mathf.Cos(shotAngle), Mathf.Sin(shotAngle));
+
+                    Instantiate(_spikeBullet, _head.position, Quaternion.identity).Shoot(shotDir);
+                }
+
+                if (coroutine != null)
+                    StopCoroutine(coroutine);
+                coroutine = StartCoroutine(BlinkCo(0.2f));
+                Sequence seq = DOTween.Sequence();
+                seq.Append(transform.DOScale(Vector3.one * 1.2f, 0.2f).SetEase(Ease.OutElastic));
+                seq.Append(transform.DOScale(Vector3.one, 0.2f).SetEase(Ease.InBounce));
+
+                _isShotCross = !_isShotCross;
+            }, 1f);
+
+            _currentShotCount++;
+        }
+
+        
+        _head.position += _dir * _turnSpeed * Time.deltaTime;
+    }
+    private void TightSnakePattern()
+    {
+        // step1
+        if (Vector3.Distance(_head.position, _worldCenterPos.position) > 15.0f && _angle < _moveAngle)
+        {
+            _head.position += _dir * _tightSnakeMoveSpeed * Time.deltaTime;
+        }
+        // step2
+        else if (_angle < _moveAngle)
+        {
+            _angle += Mathf.Deg2Rad * _rotateAngleSpeed * Time.deltaTime;
+            Vector3 pos = _worldCenterPos.position + new Vector3(Mathf.Cos(_angle), Mathf.Sin(_angle), 0) * 14.9f;
+            _dir = (pos - _head.position).normalized;
+            _head.position = pos; 
+        }
+        // step3
+        else if (Vector3.Distance(_head.position, _worldCenterPos.position) < 120.0f)
+        {
+            _head.position += _dir * _tightSnakeMoveSpeed * Time.deltaTime;
+        }
+        else
+        {
+            _isOutside = true;
+            _snakeMove.ForceDestroyBody();
+            SetCoolTime(0.5f);
+        }
+        
 
     }
 
@@ -133,29 +354,88 @@ public class SW_Pattern : MonoBehaviour
     {
         _cool -= Time.deltaTime;
         if (_cool <= 0)
-            state = ESpikeWormState.None;
+            _state = ESpikeWormState.None;
+    }
+    private IEnumerator BlinkCo(float time)
+    {
+        
+
+        _headSpriteRenderer.color = Color.white;
+        _headSpriteRenderer.material.SetFloat(HASH_BLINK, 1f);
+        yield return new WaitForSeconds(time);
+        _headSpriteRenderer.color = _saveColor;
+        _headSpriteRenderer.material.SetFloat(HASH_BLINK, 0f);
+
+
+    }
+
+    Coroutine coroutine;
+    public void ChargeCount()
+    {
+        _snakeMove.AddBody(true);
+        _currentBodyCount++;
+
+        if(coroutine != null)
+            StopCoroutine(coroutine);
+        coroutine = StartCoroutine(BlinkCo(0.1f));
+        Sequence seq = DOTween.Sequence();
+        seq.Append(transform.DOScale(Vector3.one * 1.2f, 0.1f).SetEase(Ease.OutElastic));
+        seq.Append(transform.DOScale(Vector3.one, 0.1f).SetEase(Ease.InBounce));
     }
     public void SetBossState(ESpikeWormState state)
     {
-        if (this.state == ESpikeWormState.Cool)
+        if (this._state == ESpikeWormState.Cool)
             return;
 
         switch (state)
         {
             case ESpikeWormState.SnakeMove:
-                _snakeMove.AddBody(30);
-                _head.position = _startPos.position;
+                _head.position = _underStartPos.position;
+                _snakeMove.AddBody(Vector3.down, _bodyCount);
+                break;
+            case ESpikeWormState.SpikeBullet:
+                _movePos = _worldCenterPos.position + new Vector3(Mathf.Cos(_angle), Mathf.Sin(_angle), 0) * 5f;
+                _dir = (_movePos - _head.position).normalized;
+                break;
+            case ESpikeWormState.TightSnakeBody:
+                {
+                    _angle = Random.Range(0f, 360f) * Mathf.Deg2Rad;
+                    _moveAngle = _angle + (360f * Mathf.Deg2Rad);
+                    Vector3 pos = _worldCenterPos.position + new Vector3(Mathf.Cos(_angle), Mathf.Sin(_angle), 0) * 30;
+                    _head.position = pos;
+                    _dir = (_worldCenterPos.position - _head.position).normalized;
+                    _snakeMove.AddBody(-_dir, _bodyCount);
+                }
+                break;
+            case ESpikeWormState.EnergeCharge:
+                {
+                    for(int i = 0; i < _bodyCount; ++i)
+                    {
+                        _angle = Random.Range(0f, 360f) * Mathf.Deg2Rad;
+                        Vector3 pos = _worldCenterPos.position + new Vector3(Mathf.Cos(_angle), Mathf.Sin(_angle), 0) * (30+i*5);
+
+                        Charge_SWPart spawnChargeObject = Instantiate(_chargeObject, pos, Quaternion.identity);
+                        //spawnChargeObject.transform.SetParent(transform);
+                        spawnChargeObject.SetPartInfo(_head, _chargeObjectDamage, _chargeObjectSpeed);
+                    }
+
+                    _movePos = _worldCenterPos.position + new Vector3(Mathf.Cos(_angle), Mathf.Sin(_angle), 0) * 10f;
+                }
                 break;
         }
-        this.state = state;
+        this._state = state;
     }
 
 #if UNITY_EDITOR
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.blue;
-        Gizmos.DrawWireSphere(_head.position, _radius);
-        
+        Gizmos.DrawWireSphere(_head.position, _detectRadius);
+
+        Gizmos.color = Color.green;
+        Gizmos.DrawLine(_head.position, _movePos);
+        Gizmos.DrawWireCube(_movePos, Vector3.one);
+
     }
 #endif
 }
--- Assets/02_Script/Boss/SWBoss/SnakeMove.cs
+++ Assets/02_Script/Boss/SWBoss/SnakeMove.cs
@@ -1,3 +1,4 @@
+using FD.Dev;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -18,6 +19,9 @@ public class SnakeMove : MonoBehaviour
     [SerializeField]
     private List<Transform> _tailList = new List<Transform>();
 
+    public List<Transform> BodyList => _bodyList;
+    public List<Transform> TailList => _tailList;
+
     [Header("Value")]
     [SerializeField]
     private float _bodyMinInterval = 1f;
@@ -35,10 +39,14 @@ public class SnakeMove : MonoBehaviour
     [Header("Object")]
     [SerializeField]
     private GameObject _bodyObject;
+    [SerializeField]
+    private string _bodyObject_SpriteObjectName;
+    private readonly int HASH_BLINK = Shader.PropertyToID("_StrongTintFade");
 
     private void Awake()
     {
         _startPos = _head.position;
+        
     }
 
     private void Update()
@@ -49,7 +57,7 @@ public class SnakeMove : MonoBehaviour
             DestroyMove();
 
         if (Input.GetKeyDown(KeyCode.L))
-            AddBody();
+            AddBody(Vector3.zero);
         else if (Input.GetKeyDown(KeyCode.K))
             DestroyBody();
     }
@@ -172,40 +180,101 @@ public class SnakeMove : MonoBehaviour
             p1.z = 0;
             p2.z = 0;
             Vector3 BackToFrontDir = (p1 - p2).normalized;
-            if (BackToFrontDir == Vector3.zero)
-                continue;
+            if (distance > _bodyMaxInterval)
+            {
+                backPart.position = frontPart.position - (BackToFrontDir * _bodyMaxInterval);
+            }
+            else if (BackToFrontDir == Vector3.zero)
+                break;
 
             backPart.up = BackToFrontDir;
             if (distance < _bodyMinInterval)
             {
                 backPart.position = frontPart.position - (BackToFrontDir * _bodyMinInterval);
             }
-            else if (distance > _bodyMaxInterval)
-            {
-                backPart.position = frontPart.position - (BackToFrontDir * _bodyMaxInterval);
-            }
 
             frontPart = backPart;
         }
     }
 
-    public void AddBody(int cnt = 1)
+    public void AddBody(Vector3 dir, int cnt = 1)
     {
         for(int i = 0; i < cnt; i++) 
         {
             GameObject backPart = Instantiate(_bodyObject, _bodyRootTrm);
+
             if (_bodyList.Count == 0)
                 backPart.transform.position = _head.position;
             else
                 backPart.transform.position = _bodyList[_bodyList.Count - 1].position;
+            backPart.transform.position += dir * _bodyMinInterval;
+
             _bodyList.Add(backPart.transform);
         }
         
     }
-
-    public void DestroyBody()
+    public void AddBody(bool spawnLastBodyDir = false)
     {
+        Vector3 dir = Vector3.zero;
+        if (spawnLastBodyDir && _bodyList.Count >= 2)
+            dir = (_bodyList[_bodyList.Count - 1].position - _bodyList[_bodyList.Count - 2].position).normalized;
+        else if (spawnLastBodyDir && _bodyList.Count >= 1)
+            dir = (_bodyList[0].position - _head.position).normalized;
+
+        GameObject backPart = Instantiate(_bodyObject, _bodyRootTrm);
+        if (string.IsNullOrEmpty(_bodyObject_SpriteObjectName) == false)
+        {
+
+            Transform findObject = backPart.transform.Find(_bodyObject_SpriteObjectName);
+            SpriteRenderer backPartSprite = findObject?.GetComponent<SpriteRenderer>();
+            
+            if (backPartSprite != null)
+            {
+                Color saveColor = backPartSprite.color;
+                backPartSprite.color = Color.white;
+                backPartSprite.material.SetFloat(HASH_BLINK, 1f);
+
+                FAED.InvokeDelay(() =>
+                {
+                    backPartSprite.color = saveColor;
+                    backPartSprite.material.SetFloat(HASH_BLINK, 0f);
+                }, 0.8f);
+            }
+        }
+
+        if (_bodyList.Count == 0)
+            backPart.transform.position = _head.position;
+        else
+            backPart.transform.position = _bodyList[_bodyList.Count - 1].position;
+        backPart.transform.position += dir * _bodyMinInterval;
+
+        _bodyList.Add(backPart.transform);
+    }
+
+    public void DestroyBody(float destroySpeed = 0f)
+    {
+        if (destroySpeed != 0f)
+            _destroySpeed = destroySpeed;
+
         if (_isPlayedDestroy) return;
         _isPlayedDestroy = true;
+    }
+    public void ForceDestroyBody()
+    {
+        foreach(Transform body in _bodyList)
+        {
+            Destroy(body.gameObject);
+        }
+
+        foreach(Transform tail in _tailList)
+        {
+            Destroy(tail.gameObject);
+        }
+
+        _bodyList.Clear();
+        _tailList.Clear();
+
+        _bodyList = new List<Transform>();
+        _tailList = new List<Transform>();
     }
 }
