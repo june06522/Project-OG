d972da71c1430986312b9b7a69ab9c28cdb9cd71
ahslxjvhapt12 <likjhee1@gmail.com>
2024-04-16 10:47:15 +0900

    LaserGun Complete

--- Assets/02_Script/Weapon/Weapons/LaserGunLine.cs
+++ Assets/02_Script/Weapon/Weapons/LaserGunLine.cs
@@ -10,12 +10,23 @@ public class LaserGunLine : MonoBehaviour
     public LineRenderer LineRenderer => _lineRenderer;
     public EdgeCollider2D EdgeCollider => _edgeCollider;
 
+    private float damage;
 
-    public void SetLine(Vector2 startPos, Vector2 endPos)
+    public void SetLine(Vector2 startPos, Vector2 endPos, float damage)
     {
         _lineRenderer.SetPosition(0, startPos);
-        _lineRenderer.startWidth = 1;
+        _lineRenderer.startWidth = 0.5f;
         _lineRenderer.SetPosition(1, endPos);
-        _lineRenderer.endWidth = 1;
+        _lineRenderer.endWidth = 0.5f;
+        this.damage = damage;
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent<IHitAble>(out var h))
+        {
+            h.Hit(damage);
+        }
+    }
+
 }
