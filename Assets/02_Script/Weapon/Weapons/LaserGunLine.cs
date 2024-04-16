c2624daecc541db3b782bdb4ef6e4548c1a80bee
ahslxjvhapt12 <likjhee1@gmail.com>
2024-04-16 10:14:18 +0900

    zz tlqkf

--- Assets/02_Script/Weapon/Weapons/LaserGunLine.cs
+++ Assets/02_Script/Weapon/Weapons/LaserGunLine.cs
@@ -7,6 +7,15 @@ public class LaserGunLine : MonoBehaviour
     [SerializeField] LineRenderer _lineRenderer;
     [SerializeField] EdgeCollider2D _edgeCollider;
 
-    public LineRenderer LineRenderer=> _lineRenderer;
-    public EdgeCollider2D EdgeCollider=> _edgeCollider;
+    public LineRenderer LineRenderer => _lineRenderer;
+    public EdgeCollider2D EdgeCollider => _edgeCollider;
+
+
+    public void SetLine(Vector2 startPos, Vector2 endPos)
+    {
+        _lineRenderer.SetPosition(0, startPos);
+        _lineRenderer.startWidth = 1;
+        _lineRenderer.SetPosition(1, endPos);
+        _lineRenderer.endWidth = 1;
+    }
 }
