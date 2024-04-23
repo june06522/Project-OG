01349954e10840d681c44c2d3e6f393b7dae4466
Nam junseong <danielnam0801@gmail.com>
2024-04-23 10:01:57 +0900

    AddPrefab

--- Assets/04_Script/Weapon/RotateClone/LightSaborClone.cs
+++ Assets/04_Script/Weapon/RotateClone/LightSaborClone.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LaserSaborClone : RotateClone
+public class LightSaborClone : RotateClone
 {
     protected virtual void OnTriggerEnter2D(Collider2D collision)
     {
