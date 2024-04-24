d7d88f0562baaae83250dab88a65a37b81fc32b4
ksan09 <kansan060223@gmail.com>
2024-04-24 20:17:53 +0900

    Add Feedback

---
+++ Assets/04_Script/Feedback/Feedbacks/FeedbackObects/ParticleObject.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(ParticleSystem))]
+public class ParticleObject : MonoBehaviour
+{
+    private ParticleSystem _particleSystem;
+
+    void Start()
+    {
+        _particleSystem = GetComponent<ParticleSystem>();
+    }
+
+    public void PlayOnceParticle()
+    {
+        StartCoroutine(PlayParticleCoroutine());
+    }
+
+    IEnumerator PlayParticleCoroutine()
+    {
+        _particleSystem.Play();
+
+        yield return new WaitUntil(() => !_particleSystem.isPlaying);
+
+        Destroy(gameObject);
+    }
+
+}
