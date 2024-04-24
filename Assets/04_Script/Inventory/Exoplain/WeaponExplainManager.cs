985e76d3bf5e48b42cfba50299096f32e5fcbcd3
준이 <98876187+June060522@users.noreply.github.com>
2024-04-24 18:42:13 +0900

    errordice

--- Assets/04_Script/Inventory/Exoplain/WeaponExplainManager.cs
+++ Assets/04_Script/Inventory/Exoplain/WeaponExplainManager.cs
@@ -28,21 +28,21 @@ public class WeaponExplainManager : MonoBehaviour
         #endregion
 
         #region 트리거 설명
-        weaponExplain.Add(GeneratorID.DashAttack, "대쉬 어택");
-        weaponExplain.Add(GeneratorID.DeathRay, "대쓰 레이");
-        weaponExplain.Add(GeneratorID.Electronic, "일렉트로닉");
-        weaponExplain.Add(GeneratorID.ErrorDice, "에러 다이스");
-        weaponExplain.Add(GeneratorID.Force, "포스");
-        weaponExplain.Add(GeneratorID.HeartBeat, "하트비트");
-        weaponExplain.Add(GeneratorID.LaserPointer, "레이저포인터");
-        weaponExplain.Add(GeneratorID.MagneticField, "자기장");
-        weaponExplain.Add(GeneratorID.OverLoad, "과부화");
-        weaponExplain.Add(GeneratorID.Reboot, "리부트");
-        weaponExplain.Add(GeneratorID.RotateWeapon, "로테이트 웨폰");
-        weaponExplain.Add(GeneratorID.SequenceAttack, "시퀀스어택");
-        weaponExplain.Add(GeneratorID.SiegeMode, "시즈모드");
-        weaponExplain.Add(GeneratorID.Trinity, "트포");
-        weaponExplain.Add(GeneratorID.WeaponShot, "무기샷");
+        triggerExplain.Add(GeneratorID.DashAttack,      TriggerID.Dash);
+        triggerExplain.Add(GeneratorID.DeathRay,        TriggerID.NormalAttack);
+        triggerExplain.Add(GeneratorID.ErrorDice,       TriggerID.Dash);
+        triggerExplain.Add(GeneratorID.Electronic,      TriggerID.CoolTime);
+        triggerExplain.Add(GeneratorID.Force,           TriggerID.NormalAttack);
+        triggerExplain.Add(GeneratorID.HeartBeat,       TriggerID.Dash);
+        triggerExplain.Add(GeneratorID.LaserPointer,    TriggerID.Move);
+        triggerExplain.Add(GeneratorID.MagneticField,   TriggerID.NormalAttack);
+        triggerExplain.Add(GeneratorID.OverLoad,        TriggerID.Dash);
+        triggerExplain.Add(GeneratorID.Reboot,          TriggerID.Dash);
+        triggerExplain.Add(GeneratorID.RotateWeapon,    TriggerID.Move);
+        triggerExplain.Add(GeneratorID.SequenceAttack,  TriggerID.Dash); // 얜 미정
+        triggerExplain.Add(GeneratorID.SiegeMode,       TriggerID.Idle);
+        triggerExplain.Add(GeneratorID.Trinity,         TriggerID.NormalAttack);
+        triggerExplain.Add(GeneratorID.WeaponShot,      TriggerID.CoolTime);
         #endregion
     }
 }
--- Assets/04_Script/Weapon/Skill/Skills/ErrorDice/ErrorDiceSkill.cs
+++ Assets/04_Script/Weapon/Skill/Skills/ErrorDice/ErrorDiceSkill.cs
@@ -6,20 +6,29 @@ public class ErrorDiceSkill : Skill
 {
     List<Shell> weaponList;
 
+    float curtime = 0f;
+    float time = 5f;
+
     private void Start()
     {
         weaponList = SkillContainer.Instance.GetList();
     }
 
+    private void Update()
+    {
+        curtime += Time.deltaTime;
+    }
+
     public override void Excute(Transform weaponTrm, Transform target, int power)
     {
         Skill s = null;
-        int i, j;
+        int i = 0, j = 0;
+
 
         while(s==null)
         {
             i = Random.Range(1,weaponList.Count);
-            if (weaponList[i] == null || weaponList[i].skillList.Count > 1 || i == (int)GeneratorID.ErrorDice)
+            if (weaponList[i].skillList == null || weaponList[i].skillList.Count <= 1 || i == (int)GeneratorID.ErrorDice)
                 continue;
 
             j = Random.Range(1, weaponList[i].skillList.Count);
@@ -27,6 +36,23 @@ public class ErrorDiceSkill : Skill
                 s = weaponList[i].skillList[j];
         }
 
-        s.Excute(weaponTrm,target,power);
+
+        if (WeaponExplainManager.triggerExplain[(GeneratorID)i] == TriggerID.Idle ||
+        WeaponExplainManager.triggerExplain[(GeneratorID)i] == TriggerID.Move)
+            StartCoroutine(Co(s, weaponTrm, target, power));
+        else
+            s.Excute(weaponTrm, target, power);
+    }
+
+    IEnumerator Co(Skill s,Transform weaponTrm, Transform target, int power)
+    {
+        curtime = 0.0f;
+        while(curtime < time)
+        {
+            if(target != null && weaponTrm != null)
+                s.Excute(weaponTrm, target, power);
+            yield return null;
+        }
+        yield return null;
     }
 }
