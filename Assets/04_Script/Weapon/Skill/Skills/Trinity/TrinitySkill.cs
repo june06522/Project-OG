2a6a8e8444549cdbd00801284a62417bc8ebae56
준이 <98876187+June060522@users.noreply.github.com>
2024-04-29 19:58:49 +0900

    trinity bug fix

--- Assets/04_Script/Weapon/Skill/Skills/Trinity/TrinitySkill.cs
+++ Assets/04_Script/Weapon/Skill/Skills/Trinity/TrinitySkill.cs
@@ -1,61 +1,27 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class TrinitySkill : Skill
 {
-    TrinityObject obj;
+    [SerializeField]TrinityObject trinityObj;
 
-    Dictionary<Weapon, int> damageDic = new();
-    bool isOn = false;
-    Coroutine co = null;
-    float _defaultVal = 40f;
-    float _plusVal = 10f;
+    Dictionary<Tuple<Transform,SendData>, TrinityObject> damageDic = new();
+    
 
     public override void Excute(Transform weaponTrm, Transform target, int power, SendData trigger = null)
     {
         Weapon weapon = weaponTrm.GetComponent<Weapon>();
 
-
-        if (damageDic.ContainsKey(weapon))
+        var tuple = Tuple.Create(weaponTrm, trigger);
+        if(!damageDic.ContainsKey(tuple))
         {
-            if (power != damageDic[weapon])
-            {
-                weapon.Data.AddDamege -= GetDamage(weapon, damageDic[weapon]);
-                damageDic[weapon] = power;
-                weapon.Data.AddDamege += GetDamage(weapon, damageDic[weapon]);
-            }
+            damageDic.Add(tuple, Instantiate(trinityObj));
         }
-        else
-        {
-            weapon.Data.AddDamege += GetDamage(weapon,power);
-            damageDic.Add(weapon, power);
-        }
-
-        isOn = true;
-        if (co == null)
-            co = StartCoroutine(ISkillOn());
-    }
-
-    IEnumerator ISkillOn()
-    {
-        while (isOn)
-        {
-            isOn = false;
-            yield return null;
-        }
-        co = null;
-        foreach (var v in damageDic)
-        {
-            v.Key.Data.AddDamege -= GetDamage(v.Key,v.Value);
-        }
-        damageDic.Clear();
-    }
-
-    private float GetDamage(Weapon weapon,int power)
-    {
-        float originDamage = weapon.Data.AttackDamage.GetValue();
+        damageDic[tuple].Execute(weapon, target, power);
         
-        return originDamage * ((_defaultVal + _plusVal * power) / 100);
     }
+
+    
 }
