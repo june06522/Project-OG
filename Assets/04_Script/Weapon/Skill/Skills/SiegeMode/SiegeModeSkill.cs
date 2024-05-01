666b1bed3f6bc5055d1e3ac5f2ebd300813fd288
준이 <98876187+June060522@users.noreply.github.com>
2024-05-01 19:11:59 +0900

    bugFix

--- Assets/04_Script/Weapon/Skill/Skills/SiegeMode/SiegeModeSkill.cs
+++ Assets/04_Script/Weapon/Skill/Skills/SiegeMode/SiegeModeSkill.cs
@@ -2,17 +2,25 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class SiegeModeSkill : Skill
 {
     [SerializeField] SiegeModeObj siegeModeobj;
+    [SerializeField] VisualEffect effect;
+    VisualEffect v;
 
-    Dictionary<Tuple<Transform, SendData>, SiegeModeObj> _coolDownDic = new();
+    Dictionary<Tuple<Transform, Transform>, SiegeModeObj> _coolDownDic = new();
     public override void Excute(Transform weaponTrm, Transform target, int power, SendData trigger = null)
     {
+        if(v != null)
+        {
+            v = Instantiate(effect,GameManager.Instance.player.position,Quaternion.identity);
+        }
+
         Weapon weapon = weaponTrm.GetComponent<Weapon>();
 
-        var tuple = Tuple.Create(weaponTrm, trigger);
+        var tuple = Tuple.Create(weaponTrm, trigger.trigger);
         if(!_coolDownDic.ContainsKey(tuple))
         {
             _coolDownDic.Add(tuple, Instantiate(siegeModeobj));
