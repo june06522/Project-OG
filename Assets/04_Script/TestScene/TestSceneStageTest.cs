f47cae19dc8cd0e9bb127e6fe9169620110009a3
준이 <98876187+June060522@users.noreply.github.com>
2024-05-02 15:28:36 +0900

    boss

---
+++ Assets/04_Script/TestScene/TestSceneStageTest.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestSceneStageTest : MonoBehaviour
+{
+    Stage stage;
+
+    public void SettingStage(Stage stage)
+    {
+        this.stage = stage;
+    }
+
+    public void PlayStage()
+    {
+        Instantiate(stage);
+    }
+}
