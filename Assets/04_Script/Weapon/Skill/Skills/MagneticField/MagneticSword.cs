4fc07569a1228afc0d743e1fe49808107231ee00
준이 <98876187+June060522@users.noreply.github.com>
2024-05-07 19:25:21 +0900

    bugFix

--- Assets/04_Script/Weapon/Skill/Skills/MagneticField/MagneticSword.cs
+++ Assets/04_Script/Weapon/Skill/Skills/MagneticField/MagneticSword.cs
@@ -5,6 +5,9 @@ public class MagneticSword : Skill
     [SerializeField] MagneticObject prefab;
     public override void Excute(Transform weaponTrm, Transform target, int power, SendData trigger = null)
     {
+        if (weaponTrm.GetComponent<Weapon>() != trigger.startWeapon)
+            return;
+
         var obj = Instantiate(prefab, weaponTrm.position, Quaternion.identity);
         obj.SetDamage(power * power);
     }
