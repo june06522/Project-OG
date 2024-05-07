bffa72939e2614a67f779214c2a9fbe37f14116c
준이 <98876187+June060522@users.noreply.github.com>
2024-05-07 16:06:05 +0900

    change

--- Assets/04_Script/Inventory/Event/RotateEvent.cs
+++ Assets/04_Script/Inventory/Event/RotateEvent.cs
@@ -5,4 +5,46 @@ using UnityEngine;
 public class RotateEvent : MonoBehaviour
 {
     Item _curItem;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Brick"))
+        {
+            if (_curItem == null)
+            {
+                _curItem = collision.GetComponent<Item>();
+                if(_curItem == null )
+                {
+                    Debug.LogError($"{transform} : Item Component is null");
+                    return;
+                }
+                SpendMoney();
+                _curItem.transform.position = transform.position;
+
+                
+            }
+            else
+            {
+                Vector2 pos = collision.transform.position;
+                collision.transform.position = pos;
+            }
+        }
+    }
+
+    public void LeftRotate()
+    {
+        if (_curItem == null)
+            return;
+    }
+
+    public void RightRotate()
+    {
+        if (_curItem == null)
+            return;
+    }
+
+    public void SpendMoney()
+    {
+
+    }
 }
