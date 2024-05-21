6018128564df01e31c8cd7331ce3ea302b6739f3
ksan09 <kansan060223@gmail.com>
2024-05-21 20:22:41 +0900

    Update SnakeMonster

--- Assets/04_Script/Enemy/Type/Snake/State/SnakePatrol.cs
+++ Assets/04_Script/Enemy/Type/Snake/State/SnakePatrol.cs
@@ -12,8 +12,9 @@ public class SnakePatrol : MonoBehaviour
 
     private Vector2 _movePos;
     private float _lerpValue;
-    private float _distance;
-    private float _speed = 25f;
+    private float _speed = 8f;
+    private bool _isLeftSnakeMove = false;
+    private bool _isMoveToPlayer = false;
 
     private void Awake()
     {
@@ -26,13 +27,26 @@ public class SnakePatrol : MonoBehaviour
     private void Update()
     {
         // Move
+        float distance = Vector2.Distance(_target.position, _root.position);
+        if (distance < 2f)
+        {
+            if (distance < 0.1f)
+                return;
+
+            _isMoveToPlayer = true;
+            _movePos = _target.position;
+            SetRootAngle();
+            RootMove();
+            return;
+        }
+
         WindingMove();
     }
 
     // 베지어 곡선을 활용한 이동
     private void WindingMove()
     {
-        if(Vector2.Distance(_root.position, _movePos) < 0.1f)
+        if(Vector2.Distance(_root.position, _movePos) < 0.1f || _isMoveToPlayer)
             CalculateMovePos();
 
         SetRootAngle();
@@ -49,7 +63,7 @@ public class SnakePatrol : MonoBehaviour
         }
 
         // lerp calculate
-        _lerpValue += 0.01f * _speed;
+        _lerpValue += 0.1f;
 
         _movePos = _points[0];
         for(int i = 1; i < _points.Count; ++i)
@@ -57,6 +71,7 @@ public class SnakePatrol : MonoBehaviour
             _movePos = Vector2.Lerp(_movePos, _points[i], _lerpValue);
         }
 
+        _isMoveToPlayer = false;
     }
     private void SetRootAngle()
     {
@@ -74,25 +89,22 @@ public class SnakePatrol : MonoBehaviour
         _points.Clear();
         _points.Add(_root.position);
 
-        // Winding Position
-        int windingCount = 2;
-        Vector2 dir = (_target.position - _root.position).normalized;
-        for (int i = 1; i <= windingCount; ++i)
-        {
-            float posPartValue = (1f / (windingCount+1)) * i;
-            Vector2 partPos = Vector2.Lerp(_root.position, _target.position, posPartValue);
+        Vector3 dir = (_target.position - _root.position).normalized;
+        Vector3 rotateDir = RotateDir(dir);
 
+        _points.Add(_root.position + (dir * 2f) + (rotateDir * 10f));
 
+        _points.Add(_root.position + dir * 4f);
 
-
-        }
-
-        
-
-
-        _points.Add(_target.position);
-
-        _distance = Vector2.Distance(_root.position, _target.position) + 30f;
         _lerpValue = 0;
     }
+    private Vector2 RotateDir(Vector2 dir)
+    {
+        float angle = Mathf.Atan2(dir.y, dir.x);
+        angle += ((_isLeftSnakeMove ? -90f : 90f) * Mathf.Deg2Rad);
+        _isLeftSnakeMove = !_isLeftSnakeMove;
+
+        return new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
+    }
+
 }
