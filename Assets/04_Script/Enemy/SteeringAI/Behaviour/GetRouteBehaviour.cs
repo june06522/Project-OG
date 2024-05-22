4d95c8731ac12aadc4ca4607e3d84b395d63a86d
Nam junseong <danielnam0801@gmail.com>
2024-05-23 00:03:35 +0900

    change

--- Assets/04_Script/Enemy/SteeringAI/Behaviour/GetRouteBehaviour.cs
+++ Assets/04_Script/Enemy/SteeringAI/Behaviour/GetRouteBehaviour.cs
@@ -18,19 +18,19 @@ public class GetRouteBehaviour : SteeringBehaviour
 
     public override (float[] danger, float[] interest) GetSteering(float[] danger, float[] interest, AIData aiData)
     {
-        if (aiData.PathData.IsExist() == true)
-        {
-            Vector2 getNextRouteDirNormalized = (aiData.PathData.GetFirst() - (Vector2)transform.position).normalized;
-            Debug.Log(getNextRouteDirNormalized);
-            for (int i = 0; i < Directions.eightDirections.Count; i++)
-            {
-                float result = Vector2.Dot(getNextRouteDirNormalized, Directions.eightDirections[i]);
-                interest[i] += result;
-            }
-            interestsTemp = interest;
-        }
-        else
-            interestsTemp = null;
+        //if (aiData.PathData.IsExist() == true)
+        //{
+        //    Vector2 getNextRouteDirNormalized = (aiData.PathData.GetFirst() - (Vector2)transform.position).normalized;
+        //    Debug.Log(getNextRouteDirNormalized);
+        //    for (int i = 0; i < Directions.eightDirections.Count; i++)
+        //    {
+        //        float result = Vector2.Dot(getNextRouteDirNormalized, Directions.eightDirections[i]);
+        //        interest[i] += result;
+        //    }
+        //    interestsTemp = interest;
+        //}
+        //else
+        //    interestsTemp = null;
         return (danger, interest);
     }
 
