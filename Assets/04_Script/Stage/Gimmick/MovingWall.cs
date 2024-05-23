c346a3493bcae48b7007884aeef5a9cf2e5719de
ahslxjvhapt12 <likjhee1@gmail.com>
2024-05-23 09:21:16 +0900

    MovingObject

--- Assets/04_Script/Stage/Gimmick/MovingWall.cs
+++ Assets/04_Script/Stage/Gimmick/MovingWall.cs
@@ -3,7 +3,9 @@ using UnityEngine;
 public class MovingWall : MonoBehaviour
 {
     [SerializeField] float offset;
+    [SerializeField] float speed;
     [SerializeField] Vector2 moveDir;
+    [SerializeField] LineRenderer line;
 
     Vector2 anchorPos;
     float minX;
@@ -23,12 +25,15 @@ public class MovingWall : MonoBehaviour
         minY = anchorPos.y - moveDir.y * offset;
         maxY = anchorPos.y + moveDir.y * offset;
 
+        line.SetPosition(0, new Vector2(minX, minY));
+        line.SetPosition(1, new Vector2(maxX, maxY));
+
     }
 
     private void Update()
     {
 
-        float ratio = (Mathf.Sin(Time.time) + 1f) * 0.5f;
+        float ratio = (Mathf.Sin(Time.time * speed) + 1f) * 0.5f;
 
         float x = Mathf.Lerp(minX, maxX, ratio);
         float y = Mathf.Lerp(minY, maxY, ratio);
