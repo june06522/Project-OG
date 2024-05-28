017fe070f07ccc7dfee42393fa4dee12adc5513d
ahslxjvhapt12 <likjhee1@gmail.com>
2024-05-28 20:21:11 +0900

    Enum 추가
    시너지 개발

--- Assets/04_Script/Inventory/Synergy/SynergyManager.cs
+++ Assets/04_Script/Inventory/Synergy/SynergyManager.cs
@@ -8,6 +8,11 @@ public class SynergyManager : MonoBehaviour
 
     private static SynergyManager instance;
     public static SynergyManager Instance => instance;
+
+
+    
+    Dictionary<TriggerID, int> synergyContainer;
+    
     private void Awake()
     {
 
@@ -23,14 +28,32 @@ public class SynergyManager : MonoBehaviour
 
     }
 
-    public void AddItem()
+    public void AddItem(TriggerID id)
     {
+
+        synergyContainer[id]++;
 
     }
 
-    public void RemoveItem()
+    public void RemoveItem(TriggerID id)
     {
+
+        if (synergyContainer.ContainsKey(id))
+        {
+
+            if (synergyContainer[id] != 0)
+            {
+
+                synergyContainer[id]--;
+
+            }
+
+        }
 
     }
 
+    public void UpdateStat()
+    {
+
+    }
 }
