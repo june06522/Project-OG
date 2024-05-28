1d8cb7078cb9d01f4770b5ce16cba04bce27d784
Nam junseong <danielnam0801@gmail.com>
2024-05-28 18:44:38 +0900

    change

---
+++ Assets/04_Script/Obstacle/Cannon.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using DG.Tweening;
+
+public class Cannon : MonoBehaviour
+{
+    [SerializeField] EnemyBullet bullet;
+    [SerializeField] Transform shootPoint;
+    [SerializeField] float speedRatio;
+    [SerializeField] float scaleRatio;
+    
+    [SerializeField]
+    AnimationCurve curve;
+
+    Material mat;
+    Vector3 originScale;
+    
+
+    private void Awake()
+    {
+        MeshRenderer mr = GetComponent<MeshRenderer>();
+        mat = Instantiate(mr.material);
+        mr.material = mat;
+        originScale = transform.localScale;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            Shoot();
+    }
+
+    public void Shoot() => StartCoroutine(ShootCoroutine());
+
+    public IEnumerator ShootCoroutine()
+    {
+        Vector2 dir = transform.right;
+        float start = 0f;
+        float end = 1f;
+
+        mat.SetFloat("_Panner", -0.2f);
+        while (start < end)
+        {
+            Debug.Log(Mathf.Lerp(-0.2f, 1.2f, curve.Evaluate(start / end)));
+            mat.SetFloat("_Panner", Mathf.Lerp(-0.2f, 1.2f, curve.Evaluate(start/end)));
+            start += Time.deltaTime * speedRatio;
+            yield return null;
+        }
+
+        transform.DOPunchScale(originScale * scaleRatio, 0.5f);
+        EnemyBullet clone = Instantiate(bullet, shootPoint.position, Quaternion.identity);
+        clone.Shoot(transform.up);
+    }
+}
