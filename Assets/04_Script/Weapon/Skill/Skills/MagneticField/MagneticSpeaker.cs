16373986bc5e53496136d159c961e31d34194ac1
ksan09 <kansan060223@gmail.com>
2024-06-12 19:17:39 +0900

    update

--- Assets/04_Script/Weapon/Skill/Skills/MagneticField/MagneticSpeaker.cs
+++ Assets/04_Script/Weapon/Skill/Skills/MagneticField/MagneticSpeaker.cs
@@ -5,7 +5,7 @@ public class MagneticSpeaker : Skill
     [SerializeField] MagneticObject prefab;
     public override void Excute(Transform weaponTrm, Transform target, int power, SendData trigger = null)
     {
-        if (weaponTrm.GetComponent<Weapon>() != trigger.startWeapon)
+        if (weaponTrm == null || weaponTrm.GetComponent<Weapon>() != trigger.startWeapon)
             return;
 
         var obj = Instantiate(prefab, weaponTrm.position, Quaternion.identity);
