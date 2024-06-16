12a289f8bbf222d4e2bdfd7fc1fe37484535f0d0
준이 <98876187+June060522@users.noreply.github.com>
2024-06-16 22:03:47 +0900

    change BuildSetting

--- Assets/04_Script/Tutorial/QuestManager.cs
+++ Assets/04_Script/Tutorial/QuestManager.cs
@@ -924,6 +924,6 @@ public class QuestManager : MonoSingleton<QuestManager>
 
         DataManager.Instance.tutorialData.isClear = true;
         DataManager.Instance.SaveTutorialData();
-        SceneManager.LoadScene("Tutorial");
+        SceneManager.LoadScene("Intro");
     }
 }
