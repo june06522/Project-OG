a951a5d8f08187958238a5be70d904bdda2243d9
준이 <98876187+June060522@users.noreply.github.com>
2024-07-02 22:25:41 +0900

    s

--- Assets/04_Script/Weapon/Skill/Skills/DashAttack/SaborDashSkill.cs
+++ Assets/04_Script/Weapon/Skill/Skills/DashAttack/SaborDashSkill.cs
@@ -24,6 +24,7 @@ public class SaborDashSkill : Skill
         if (coroutine != null)
             StopCoroutine(coroutine);
 
+        if (sword != null)
         coroutine = StartCoroutine(sword.ReinforceAttack(target, targetScale));
     }
 
